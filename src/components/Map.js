16a45529542209e75b191a76c5e3efd6305e6aea
Dmitry <manxxeta@gmail.com>
2018-10-16 12:58:39 -0700

    initial

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
-import '../index.css';
-
-class Map extends React.Component {
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 
 
-    render() {
 
-        const GoogleMapExample = withGoogleMap(props => {
-            <GoogleMap 
-            defaultCenter = { { lat: 40.756795, lng: -73.954298 } } 
-            defaultZoom = { 13 }
-            >
-            </GoogleMap>
-        });
-
-        return(
-            <div className='map-div'>
-            <GoogleMapExample 
-                containterElement = { <div style={{ height: `500px`, width: '500px' }} /> } 
-                mapElement = { <div style={{ height: `100%` }} /> }
-            />
-            </div>
-        );
-    }
-};
 
 export default Map;
