e437314c71c1d32038b80fe0901b675f07e3ca52
Dmitry <manxxeta@gmail.com>
2018-10-16 11:26:07 -0700

    initial

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { config } from './config.js';
+
+
+class App extends React.Component
+{
+
+    render(){
+        return (
+            <div>{config.REACT_APP_GOOGLE_MAPS_KEY}</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
