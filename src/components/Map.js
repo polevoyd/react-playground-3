7949c4130266a56e8b085f46fe9a57e035d03397
Dmitry <manxxeta@gmail.com>
2018-10-17 12:47:40 -0700

    editing

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,20 +8,55 @@ class Map extends React.Component {
 
   componentDidMount(){
 
+    // // Location Lang, Lat
+    // let locationCoordinates = [];
+
+    // // Building request and based on response create a map
+    // const linkToRequest = `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.props.placeToSearch}.json?access_token=${config.REACT_APP_MAPS_KEY}`;
     
+    // // Sending a request
+    // fetch(linkToRequest)
+    //   .then(results => results.json())
+    //   .then((results) => {
+
+    //     // For just take first and most relevant result
+    //     // results.features - array of locations
+        
+    //     this.setState({
+    //       location: results.features[0].place_name
+    //     })
+        
+    //     console.log(results.features[0].center)
+    //     // locationCoordinates = results.features[0].center;
 
 
 
 
-    // Creating and rendering a mapbox
-    mapboxgl.accessToken = config.REACT_APP_MAPS_KEY;
-    const map = new mapboxgl.Map({
-        container: document.getElementById('mapbox-container'),
-        style: 'mapbox://styles/mapbox/streets-v9',
-        center: [-122.420679, 37.772537],
-        zoom: 13,
-        hash: true 
-    });
+        
+    //   })
+
+
+
+        // // Creating and rendering a mapbox
+        // mapboxgl.accessToken = config.REACT_APP_MAPS_KEY;
+        // let map = new mapboxgl.Map({
+        //     container: document.getElementById('mapbox-container'),
+        //     style: 'mapbox://styles/mapbox/streets-v9',
+        //     center: this.state.locationCoordinates,
+        //     zoom: 13,
+        //     hash: true 
+        // });
+
+
+
+
+
+
+
+
+
+
+
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import { config } from './config';
+import {config} from './config'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LocationInput from './components/LocationInput';
@@ -14,16 +14,41 @@ class App extends React.Component {
     this.state = {
       // set initial location to current location and load it on mount
       // 'Seattle' - only for a testing simplicity
-      location: 'Seattle',
-      locationCoordinates: [0, 0]
+      location: 'Seattle'
     }
     this.handleLocationSubmit = this.handleLocationSubmit.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
   }
 
 
   // request a location by name and get back a lang and lat of that location on a map
   requestLocationGeocode(locationName) {
+    // Location Lang, Lat
+    let locationCoordinates = [];
 
+    // Building request and based on response create a map
+    const linkToRequest = `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.props.placeToSearch}.json?access_token=${config.REACT_APP_MAPS_KEY}`;
+    
+    // Sending a request
+    fetch(linkToRequest)
+      .then(results => results.json())
+      .then((results) => {
+
+        // For just take first and most relevant result
+        // results.features - array of locations
+        
+        this.setState({
+          location: results.features[0].place_name
+        })
+        
+        console.log(results.features[0].center)
+        // locationCoordinates = results.features[0].center;
+
+
+
+
+        
+      })
   }
 
   // handle location submit - change location in a state after user clicked 'Submit'
@@ -36,34 +61,19 @@ class App extends React.Component {
     // by sending request and get response
     // if yes, then change state.location to a new one
 
-    // Location Lang, Lat
-    let locationCoordinates = [];
-
-    // Building request and based on response create a map
-    const linkToRequest = `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.state.location}.json?access_token=${config.REACT_APP_MAPS_KEYgh}`;
-    
-    // Sending a request
-    fetch(linkToRequest)
-      .then(results => results.json())
-      .then((results) => {
-
-        // For just take first and most relevant result
-        // results.features - array of locations
-        
-        this.setState({
-          location: results.features[0].place_name,
-          locationCoordinates: results.features[0].center
-        })
-        
-        // console.log(results.features[0].center)
-        locationCoordinates = results.features[0].center;
-      })
    
     this.setState({
       location: event.target[0].value
     })
 
     this.requestLocationGeocode(this.state.location)
+  }
+
+  // on change will alternate status as well
+  handleLocationChange(event) {
+    this.setState({
+      location: event.target.value
+    })
   }
 
   // 0 Entry point to app
@@ -73,7 +83,7 @@ class App extends React.Component {
 
     return (
       <div>
-        <LocationInput handleLocationSubmit={this.handleLocationSubmit}/>
+        <LocationInput handleLocationSubmit={this.handleLocationSubmit} handleLocationChange={this.handleLocationChange}/>
         <MapContainer placeToSearch={this.state.location}/>
       </div>
     )
