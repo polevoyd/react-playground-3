cb93647a6b20293d6135fbd612281e90477b6258
Dmitry <manxxeta@gmail.com>
2018-10-18 15:05:59 -0700

    editing

--- src/components/Map.js
+++ src/components/Map.js
@@ -29,6 +29,18 @@ class Map extends React.Component {
 
   renderMapToScreen(){
 
+
+  // mapbox://styles/mapbox/streets-v10
+  // mapbox://styles/mapbox/outdoors-v10
+  // mapbox://styles/mapbox/light-v9
+  // mapbox://styles/mapbox/dark-v9
+  // mapbox://styles/mapbox/satellite-v9
+  // mapbox://styles/mapbox/satellite-streets-v10
+  // mapbox://styles/mapbox/navigation-preview-day-v2
+  // mapbox://styles/mapbox/navigation-preview-night-v2
+  // mapbox://styles/mapbox/navigation-guidance-day-v2
+  // mapbox://styles/mapbox/navigation-guidance-night-v2
+
     mapboxgl.accessToken = config.REACT_APP_MAPS_KEY;
     map = new mapboxgl.Map({
         container: document.getElementById('mapbox-container'),
@@ -36,7 +48,7 @@ class Map extends React.Component {
         center: ['-122.3301', '47.6038'],
         zoom: 13,
         hash: true 
-    });
+    })
   }
 
   /* ------------------------------------------------------------------------------------------- */
