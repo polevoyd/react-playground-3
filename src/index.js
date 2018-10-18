a9c3a2e956c0bdcce8f82707566449f9aa22a0cb
Dmitry <manxxeta@gmail.com>
2018-10-18 16:47:59 -0700

    editing

--- src/index.js
+++ src/index.js
@@ -75,6 +75,7 @@ class App extends React.Component {
     
     // starting to search for venue
     this.searchForVenues();
+    
   }
 
   /* ------------------------------------------------------------------------------------------- */
