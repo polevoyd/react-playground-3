46048362b9d2f6abd8207a04b88fab04e31be6fb
Dmitry <manxxeta@gmail.com>
2018-10-22 15:17:33 -0700

    creating pins

---
+++ src/components/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css';
+
+class Popup extends React.Component {
+
+
+    render() {
+        return (
+            <div className>
+
+            </div>
+        );
+    }
+}
