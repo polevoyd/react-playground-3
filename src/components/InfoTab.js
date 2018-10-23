56dde502fdbd1ea85d6a5612eda226d1803b6c09
Dmitry <manxxeta@gmail.com>
2018-10-23 14:43:07 -0700

    creating pins

---
+++ src/components/InfoTab.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+
+class InfoTab extends React.Component {
+
+    render() {
+        return(
+            <div className="info-tab"></div>
+        );
+    }
+}
+
+export default InfoTab;
