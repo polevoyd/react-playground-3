148523abc1d5574af072d6cb3b83939fd5bb725a
Dmitry <manxxeta@gmail.com>
2018-10-23 14:11:55 -0700

    creating pins

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,7 +6,7 @@ class Info extends React.Component {
     render(){
         return(
             <div className="info-icon">
-            
+            <h1>?</h1>
             </div>
         );
     }
