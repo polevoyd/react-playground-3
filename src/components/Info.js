b37aa7da468a66606d957173b432233f4622de09
Dmitry <manxxeta@gmail.com>
2018-10-23 13:42:22 -0700

    creating pins

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+
+class Info extends React.Component {
+
+    render(){
+        return(
+            <div className="info-icon">
+            
+            </div>
+        );
+    }
+}
