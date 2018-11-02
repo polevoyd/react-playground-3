140c15170fb23605933b3bae937b89d1fd9bb80c
Dmitry <manxxeta@gmail.com>
2018-11-02 16:00:22 -0700

    rdx refactoring

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,6 +2,10 @@ const LOCATION_SET_CITY_COORDINATES = 'LOCATION_SET_CITY_COORDINATES';
 const LOCATION_SET_CITY = 'LOCATION_SET_CITY';
 const SET_TWEETS = 'SET_TWEETS';
 
+/**
+ * actions
+ */
+
 export function setLocationAndCoordinates(location, locationLngLat) {
     return { type: LOCATION_SET_CITY_COORDINATES, location, locationLngLat }
 } 
