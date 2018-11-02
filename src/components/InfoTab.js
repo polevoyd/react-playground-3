e6cad2fddff8f342c6e31eb7aa49143998b240b5
Dmitry <manxxeta@gmail.com>
2018-11-02 13:43:29 -0700

    rdx refactoring

--- src/components/InfoTab.js
+++ src/components/InfoTab.js
@@ -2,15 +2,13 @@ import React from 'react';
 import '../index.css';
 
 const InfoTab = () => {
-
-    return(
-        <div className="info-tab">
-        <h2>City Tweets</h2>
-        <p>Simply put a city and explore local tweets!</p>
-        <p>App shows you most recent and popular tweets combined.</p>
-        <p className='made-by'><a href="http://www.polevoy.in">www.polevoy.in</a></p>
-        </div>
-    );
+        return(
+            <div className="info-tab">
+            <h2>City Tweets</h2>
+            <p>Simply put a city and explore most recent and popular local tweets local tweets!</p>
+            <p className='made-by'><a href="http://www.polevoy.in">www.polevoy.in</a></p>
+            </div>
+        );
 }
 
 export default InfoTab;
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,12 +2,15 @@ import React from 'react';
 import '../index.css';
 import Map from './Map';
 
-const MapContainer = () => {
-    return (
-        <div id='mapbox-container'>
-        <Map coordinates={this.props.coordinates} handleCenterChange={this.props.handleCenterChange} tweets={this.props.tweets}/>
-    </div>
-    )
+class MapContainer extends React.Component {
+
+    render() {
+        return (
+            <div id='mapbox-container'>
+                <Map coordinates={this.props.coordinates} handleCenterChange={this.props.handleCenterChange} tweets={this.props.tweets}/>
+            </div>
+        )
+    }
 }
 
 export default MapContainer;
