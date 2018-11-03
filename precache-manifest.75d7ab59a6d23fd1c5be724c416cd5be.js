d0675e7d808b5d8cde8fe7ff448114c62ff9f5cd
Dmitry <manxxeta@gmail.com>
2018-11-02 17:36:30 -0700

    Updates

---
+++ precache-manifest.75d7ab59a6d23fd1c5be724c416cd5be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "251a21cec35249134bda",
+    "url": "/react-playground-3/static/js/runtime~main.251a21ce.js"
+  },
+  {
+    "revision": "c48a6575439f31b783c0",
+    "url": "/react-playground-3/static/js/main.c48a6575.chunk.js"
+  },
+  {
+    "revision": "23e026edc200104225b8",
+    "url": "/react-playground-3/static/js/1.23e026ed.chunk.js"
+  },
+  {
+    "revision": "c48a6575439f31b783c0",
+    "url": "/react-playground-3/static/css/main.00aa9be9.chunk.css"
+  },
+  {
+    "revision": "4740e5facf97afd0aa5bd411716a6e61",
+    "url": "/react-playground-3/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-playground-3/precache-manifest.319eab6b6acc08c88da6093dfbb1a547.js"
+  "/react-playground-3/precache-manifest.75d7ab59a6d23fd1c5be724c416cd5be.js"
 );
 
 workbox.clientsClaim();
